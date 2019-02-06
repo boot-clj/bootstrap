ee946d2c2fc18cd5483c8d80b327a5d3db208304
Matthew Ratzke <matt@degree9.io>
2019-02-06 11:26:02 -0700

    Delete exceptions.clj

--- src/boot/exceptions.clj
+++
@@ -1,7 +0,0 @@
-(ns boot.exceptions)
-
-(defn- exception-url [code]
-  (format "https://github.com/boot-clj/boot/wiki/Boot-Exceptions#%s" code))
-
-(defn security [msg code]
-  (throw (SecurityException. (format "%s See %s" msg (exception-url code)))))
