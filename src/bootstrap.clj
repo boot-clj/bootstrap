66b561a0266e3c472b2bd1486e41765d6efa35a5
Matthew Ratzke <matt@degree9.io>
2019-02-06 11:25:13 -0700

    rename file

--- src/bootstrap.clj
+++ src/bootstrap.clj
@@ -1,9 +1,11 @@
-(ns boot
+(ns bootstrap
   (:require [clojure.data.json :as json]
             [clojure.java.io :as io]
-            [boot.config :as conf]
+            [clojure.string :as str]
+            [bootstrap.config :as conf]
+            [boot.cli :as cli]
             [boot.properties :as props]
-            [boot.exceptions :as ex])
+            [bootstrap.feature :as feature])
   (:import  [java.io File]
             [java.util List])
   (:gen-class))
@@ -11,15 +13,13 @@
 ;; GraalVM ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (set! *warn-on-reflection* true) ;; warnings will result in runtime crashes
 
+(def boot-url "https://boot-clj.com")
+
 (defn- jvm-home []
   (or (System/getenv "GRAALVM_HOME") (System/getenv "JAVA_HOME")))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Boot Internal Functions ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defn- prevent-root [{:keys [boot-as-root user-name] :as config}]
-  (when-not (and (= "root" user-name) (#{"true" "yes" "1"} boot-as-root))
-    (ex/security "Refusing to run as user \"root\"." "boot-as-root")))
-
 (defn- latest-version []
   (-> "https://clojars.org/api/artifacts/boot"
     (slurp)
@@ -49,9 +49,8 @@
       (download-file url jar))))
 
 (defn- pin-version [version]
-  (let [props   (io/file (conf/work-dir) "boot.properties")]
-    (when (and (:boot-version-pin (conf/config))
-               (.exists ^File (io/file (conf/work-dir) "boot.properties"))
+  (let [props (io/file (conf/work-dir) "boot.properties")]
+    (when (and (.exists ^File (io/file (conf/work-dir) "boot.properties"))
                (not= version (:boot-version (conf/project))))
       (println (format "Pinning BOOT_VERSION to %s..." version))
       (-> (props/load-properties props)
@@ -62,16 +61,30 @@
         command (:boot-java-command (conf/config) "java")
         ^List args (into [command "-jar" jar] args)]
     (.waitFor (.start (.inheritIO (ProcessBuilder. args))))))
+
+(defn- print-version [config]
+  (let [kw->env (fn [k] (-> (name k) (str/upper-case) (str/replace "-" "_")))]
+    (println (format "%s - the clojure build tool." boot-url))
+    (doseq [key [:boot-version :boot-clojure-version :boot-clojure-name]]
+      (println (format "%s=%s" (kw->env key) (key config))))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Boot Main ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defn -main [& args]
   ;; GraalVM - needed until graalvm can include within the native image;;;;;;;;;
   (System/setProperty "java.library.path" (str (jvm-home) "/jre/lib/amd64"))
-  (let [{:keys [boot-version] :as config} (conf/config) ;; load config
-        version (parse-version boot-version)]
-    (prevent-root config)                           ;; prevent running as root
-    (download-version version)                 ;; download boot to cache
-    (pin-version version)                      ;; pin version to project
-    (launch-version version args)))            ;; launch jvm cache jar
+  (let [config  (conf/config)
+        version (parse-version (:boot-version config))
+        {opts :options :as args} (cli/parse-opts args)]
+    (println opts)
+    (cond (:version opts) (print-version config)
+      :else (feature/when-feature config
+              {::feature/allow-root "Boot is refusing to run as user \"root\"."}
+              (feature/when-feature opts              ;; download boot to cache
+                {::feature/online ::feature/skip}     ;; skip when offline
+                (download-version version))
+              (feature/when-feature config            ;; pin version to project
+                {::feature/auto-pin ::feature/skip}   ;; skip error message
+                (pin-version version))
+              (launch-version version args)))))       ;; launch jvm cache jar
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
